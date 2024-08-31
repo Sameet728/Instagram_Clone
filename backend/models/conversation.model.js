f20e39793987a05e21046654af5b93ec7f0c4e1f
Sameet <125867736+Sameet728@users.noreply.github.com>
2024-08-31 14:42:32 +0530

    Delete backend directory

--- backend/models/conversation.model.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-const conversationschema = new mongoose.Schema({
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  message: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }]
-});
-export const Conversation = mongoose.model('Conversation', conversationschema);
