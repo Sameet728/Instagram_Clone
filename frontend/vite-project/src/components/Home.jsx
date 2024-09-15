c60a353da65c1709a42a11e5bc9f8f144bbbbc0c
Sameet <125867736+Sameet728@users.noreply.github.com>
2024-09-15 14:31:14 +0530

    Update Home.jsx

--- frontend/vite-project/src/components/Home.jsx
+++ frontend/vite-project/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Feed from './feed'
+import Feed from './Feed'
 import { Outlet } from 'react-router-dom'
 import RightSidebar from './RightSidebar'
 import useGetAllPost from '@/hooks/useGetAllPost'
